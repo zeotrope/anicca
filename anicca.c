c853125cff39c22425656fa415d1b8e85c11e892
Sami Abdin <samiabdin@gmail.com>
2010-06-17 16:43:57 -0400

    Revert "made print recursive, so printing boxed values is possible"
    This reverts commit fbcf4e4b50c2f4f862890552a2baf10583bddad0.
    
    Better to merge than to cherry-pick
    
    Signed-off-by: Sami Abdin <samiabdin@gmail.com>

--- anicca.c
+++ anicca.c
@@ -9,12 +9,6 @@ V print(A y) {
     C *cv;
     I *iv;
     D *fv;
-    A *bv;
-
-    if (!y) {
-        printf("NULL");
-        return;
-    }
     switch (AT(y)) {
     case BOOL: {
         cv = (B *)AV(y);
@@ -36,25 +30,7 @@ V print(A y) {
         DO(AN(y), printf("%lf ", fv[i]));
         break;
     }
-    case BOX: {
-        bv = (A *)AV(y);
-        /* TODO?: fancy line drawings */
-        DO(AN(y), printf("(<"); print(bv[i]);
-           printf(")%s", (i+1 == AN(y))?"":","));
-        break;
     }
-    case MARK: {
-        printf("MARK");
-        break;
-    }
-    default: {
-        printf("HUH?");
-        break;
-    }
-    }
-}
-V println(A y) {
-    print(y);
     printf("\n");
 }
 
@@ -65,13 +41,8 @@ V a_init(V) {
 int main() {
     A x, y, z;
     char *s = "1 2 3";
-
-    a_init();
-
     x = array_str(strlen(s)+1, s);
     y = token_index(x);
-    println(y);
-    z = tokens(y, x);
-    println(z);
+    print(y);
     return 0;
 }
